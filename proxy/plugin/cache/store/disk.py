e96063c420ae7557b02d9081e1f8c8c0b70ad39a
Paul <21146452+tsarpaul@users.noreply.github.com>
2021-02-05 21:35:47 +0200

    Update disk.py

--- proxy/plugin/cache/store/disk.py
+++ proxy/plugin/cache/store/disk.py
@@ -36,10 +36,9 @@ class OnDiskCacheStore(CacheStore):
         self.cache_file = open(self.cache_file_path, "wb")
 
     def cache_request(self, request: HttpParser) -> Optional[HttpParser]:
-        def cache_request(self, request: HttpParser) -> Optional[HttpParser]:
-            if self.cache_file:
-                self.cache_file.write(request.build())
-                self.cache_file.write(b'\n'+b'-'*30+b'\n')
+        if self.cache_file:
+            self.cache_file.write(request.build())
+            self.cache_file.write(b'\n'+b'-'*30+b'\n')
         return request
 
     def cache_response_chunk(self, chunk: memoryview) -> memoryview:
